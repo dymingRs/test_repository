43ead705224b9a0cfa1fbc6f5a7493525d892361
dymingRs <101853130+dymingRs@users.noreply.github.com>
2024-06-19 14:39:41 +0800

    Delete VGdemoKit directory

--- VGdemoKit/Tools/Crypto/mac_aes.h
+++
@@ -1,7 +0,0 @@
-#ifndef  _MAC_AES_H_
-#define _MAC_AES_H_
-
-bool mac_aes_encrypt( uint8_t *buffer, int16_t size, uint32_t address, uint8_t type,  uint8_t frameCounter, const uint8_t key[16] );
-bool mac_aes_decrypt( uint8_t *sbuffer, int16_t size, const uint8_t key[16] );
-
-#endif
--- VGdemoKit/Tools/MAC/mac_phy.h
+++
@@ -1,6 +0,0 @@
-#ifndef _MAC_INTERFACE_H_
-#define _MAC_INTERFACE_H_
-
-//States_t mac_states_get();
-
-#endif
